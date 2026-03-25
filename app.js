b26c728ce49c542745e48907f414933db3f216a5
Matheus Teixeira De Queiroz <mateus.tq11@gmail.com>
2026-03-25 19:00:02 -0300

    Primeiro commit desse projeto

---
+++ app.js
@@ -0,0 +1,30 @@
+import tabelaBrasil from "./tabela.js";
+import express, { request } from 'express';
+
+const app = express()
+
+/* Traz a informação de todos os times */
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send(tabelaBrasil)
+})
+
+/* Busca as informações de um time de acordo com a sigla: */
+
+app.get('/:sigla', (request, response) => {
+    const siglaInformada = request.params.sigla.toUpperCase()
+    const time = tabelaBrasil.find((infoTime) => infoTime.sigla === siglaInformada)
+    response.send(time)
+})
+
+/* Busca as informações de um time de acordo com o nome: */
+
+app.get('/:nome', (request, response) => {
+    const timeInformado = request.params.nome
+    const time = tabelaBrasil.find((timeInfo) => timeInfo.nome === timeInformado)
+    response.send(time)
+})
+
+/* Cria um servidor com a porta 300 e exibirá uma mensagem 'Servidor rodando com sucesso!!!' */
+
+app.listen(300, () => console.log("Servidor rodando com sucesso!!!"))
